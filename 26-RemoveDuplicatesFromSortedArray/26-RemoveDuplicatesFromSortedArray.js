ae41806cdb5b14cd67f76912981539b4face9a22
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-06-10 09:27:56 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,18 @@
+// Last updated: 6/10/2026, 9:27:55 AM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let index = 1;
+    for(let i = 0; i < nums.length - 1; i++)
+    {
+        if(nums[i] != nums[i + 1])
+        {
+            nums[index] = nums[i + 1];
+            index++;
+            
+        }
+    }
+    return index;
+};
