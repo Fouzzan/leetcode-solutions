0f75d0c04e73aa9a0c45b385e87c51f1a8d4a7f6
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-07-08 16:46:04 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.js
@@ -0,0 +1,34 @@
+// Last updated: 7/8/2026, 4:46:03 PM
+1/**
+2 * @param {number} n
+3 * @return {number}
+4 */
+5var fib = function(n) {
+6    let a = 0;
+7    let b = 1;
+8
+9    let sum = 0;
+10    let fib = [];
+11
+12    if(n < 2){
+13            return n
+14        }
+15    else{
+16          for (let i = 2; i <= n; i++){
+17        
+18        sum = a + b ;
+19        a = b;
+20        b = sum;
+21        
+22
+23
+24
+25        
+26    }
+27    }
+28  
+29    console.log(a);
+30        console.log(b);
+31    return sum;
+32    
+33};
