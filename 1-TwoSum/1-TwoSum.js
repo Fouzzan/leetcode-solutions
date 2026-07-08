ccc73d74132ec5e345ec8b75c19399e82a2e68b8
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-07-08 11:47:31 +0530

    File updated at 7/8/2026, 11:47:29 AM

--- 1-TwoSum/1-TwoSum.js
+++ 1-TwoSum/1-TwoSum.js
@@ -1,15 +1,19 @@
-// Last updated: 7/8/2026, 11:33:43 AM
+// Last updated: 7/8/2026, 11:47:29 AM
 1/**
 2 * @param {number[]} nums
 3 * @param {number} target
 4 * @return {number[]}
 5 */
 6var twoSum = function(nums, target) {
-7    for (let i = 0; i< nums.length - 1; i++){
-8        for(j= i+1; j < nums.length; j++){
-9            if(nums[i] + nums[j] === target){
-10                return [i,j]
-11            }
-12        }
-13    }
-14};
+7    let map = new Map();
+8    for(let i = 0; i < nums.length; i++){
+9        
+10
+11        let need = target - nums[i];
+12
+13        if(map.has(need)){
+14            return [map.get(need),i]
+15        }
+16        map.set(nums[i],i);
+17    }
+18};
