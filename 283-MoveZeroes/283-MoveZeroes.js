a1bcbb88a3ec0bd566ba655e37285aee0f35c582
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-07-08 16:09:34 +0530

    File updated at 7/8/2026, 4:09:33 PM

--- 283-MoveZeroes/283-MoveZeroes.js
+++ 283-MoveZeroes/283-MoveZeroes.js
@@ -1,24 +1,23 @@
-// Last updated: 6/14/2026, 12:13:53 AM
+// Last updated: 7/8/2026, 4:09:33 PM
 1/**
 2 * @param {number[]} nums
 3 * @return {void} Do not return anything, modify nums in-place instead.
 4 */
 5var moveZeroes = function(nums) {
 6    let insertPos = 0;
-7   for(let i = 0; i < nums.length; i++ )
-8   {
-9        if(nums[i] !== 0)
-10        {
-11            nums[insertPos] = nums[i];
-12            insertPos++;
-13        }
-14        
-15   }
-16   while(insertPos < nums.length)
-17   {
-18    nums[insertPos] = 0;
-19    insertPos++;
-20   }
-21   
-22
-23};
+7
+8    for(let i = 0; i < nums.length; i++){
+9        if(nums[i] !== 0){
+10            nums[insertPos] = nums[i];
+11            insertPos++;
+12        }
+13        
+14
+15        
+16    }
+17    while(insertPos < nums.length){
+18        nums[insertPos] = 0;
+19        insertPos++
+20    }
+21
+22};
