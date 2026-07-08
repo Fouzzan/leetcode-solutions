b515003c6055b002ac6bd7bea70fda746710db6f
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-07-08 13:56:35 +0530

    File updated at 7/8/2026, 1:56:33 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.js
+++ 217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -1,15 +1,9 @@
-// Last updated: 7/8/2026, 1:53:52 PM
+// Last updated: 7/8/2026, 1:56:33 PM
 1/**
 2 * @param {number[]} nums
 3 * @return {boolean}
 4 */
 5var containsDuplicate = function(nums) {
-6    let set = [...new Set(nums)];
-7    if(set.length === nums.length){
-8        return false
-9    }
-10    else
-11    {
-12        return true
-13    }
-14};
+6    let set = new Set(nums);
+7    return set.size !== nums.length;
+8};
