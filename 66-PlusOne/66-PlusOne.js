0b1a7485a94867500de41aa83d8a31dfe6dd101d
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-07-16 16:02:17 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.js
@@ -0,0 +1,26 @@
+// Last updated: 7/16/2026, 4:02:17 PM
+1/**
+2 * @param {number[]} digits
+3 * @return {number[]}
+4 */
+5var plusOne = function(digits) {
+6    
+7   for (let i = digits.length - 1; i >= 0; i --){
+8        if(digits[i] < 9){
+9            digits[i] += 1
+10            return digits
+11        }
+12        
+13            digits[i] = 0
+14   }
+15        digits.unshift(1)
+16        return digits
+17
+18   }
+19
+20    
+21
+22    
+23
+24    
+25
