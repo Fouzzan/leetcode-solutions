af3d154b835e91ee7a09d3b4d62e5c58f90ff8f3
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-07-27 23:06:03 +0530

    File updated at 7/27/2026, 11:06:02 PM

--- 704-BinarySearch/704-BinarySearch.js
+++ 704-BinarySearch/704-BinarySearch.js
@@ -1,9 +1,27 @@
-// Last updated: 7/27/2026, 10:53:18 PM
+// Last updated: 7/27/2026, 11:06:02 PM
 1/**
 2 * @param {number[]} nums
 3 * @param {number} target
 4 * @return {number}
 5 */
 6var search = function(nums, target) {
-7    return (nums.includes(target))?(nums.indexOf(target)):(-1)
-8};
+7   let left = 0;
+8   let right = nums.length - 1;
+9
+10   while (left <= right){
+11    let middle = Math.floor((left + right) / 2);
+12    if (nums[middle] === target){
+13        return middle
+14    }
+15    else if(target > nums[middle]){
+16        left = middle + 1;
+17    }
+18    else{
+19        right = middle - 1;
+20    }
+21   }
+22
+23   return -1
+24
+25
+26};
