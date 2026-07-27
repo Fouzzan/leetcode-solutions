9e157fdfc36b96eb3a988a7e1e391fb75f9bf0df
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-07-27 22:51:19 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.js
@@ -0,0 +1,9 @@
+// Last updated: 7/27/2026, 10:51:18 PM
+1/**
+2 * @param {number[]} nums
+3 * @param {number} target
+4 * @return {number}
+5 */
+6var search = function(nums, target) {
+7    return (nums.includes(target))?(nums.indexOf(target)):(-1)
+8};
