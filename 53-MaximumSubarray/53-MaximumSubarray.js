c9f435a93c3b4e6f9fe128a4c64fe49304e61039
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-08 04:14:17 +0530

    File updated at 8/8/2026, 4:14:16 AM

--- 53-MaximumSubarray/53-MaximumSubarray.js
+++ 53-MaximumSubarray/53-MaximumSubarray.js
@@ -1,4 +1,4 @@
-// Last updated: 8/8/2026, 4:13:43 AM
+// Last updated: 8/8/2026, 4:14:16 AM
 1/**
 2 * @param {number[]} nums
 3 * @return {number}
@@ -6,18 +6,13 @@
 5var maxSubArray = function(nums) {
 6    let total = nums[0];
 7    let sum = 0;
-8    if(nums.length === 1){
-9        return nums[0]
-10    }
+8    // if(nums.length === 1){
+9    //     return nums[0]
+10    // }
 11    for (let i = 0; i < nums.length; i++) {
 12        sum += nums[i];
-13
-14        sum = (sum < nums[i]? nums[i]: sum);
-15        
-16   
-17        total = Math.max(total, sum);
-18    
-19    
-20    }
-21    return total;
-22};
+13        sum = (sum < nums[i]? nums[i]: sum);
+14        total = Math.max(total, sum);
+15    }
+16    return total;
+17};
