1af99ae20f9732c921bf3a80b0319ad460e5255f
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-08 22:28:51 +0530

    File updated at 8/8/2026, 10:28:48 PM

--- 1512-NumberofGoodPairs/1512-NumberofGoodPairs.js
+++ 1512-NumberofGoodPairs/1512-NumberofGoodPairs.js
@@ -1,13 +1,13 @@
-// Last updated: 8/8/2026, 10:25:47 PM
+// Last updated: 8/8/2026, 10:28:48 PM
 1/**
 2 * @param {number[]} nums
 3 * @return {number}
 4 */
 5var numIdenticalPairs = function(nums) {
 6    let count = 0;
-7    for(let i = 0; i < nums.length; i++){
-8        for(let j = 0; j < nums.length; j++){
-9            if(nums[i] === nums[j] && i < j){
+7    for(let i = 0; i < nums.length - 1; i++){
+8        for(let j = i + 1; j < nums.length; j++){
+9            if(nums[j] === nums[i]){
 10                count++;
 11            }
 12        }
