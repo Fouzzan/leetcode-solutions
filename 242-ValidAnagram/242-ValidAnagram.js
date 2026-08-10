d9441ccff3dd2c945b4ff97320cfad32fa48ce70
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-10 13:15:55 +0530

    File updated at 8/10/2026, 1:15:54 PM

--- 242-ValidAnagram/242-ValidAnagram.js
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -1,19 +1,16 @@
-// Last updated: 7/29/2026, 5:03:26 PM
+// Last updated: 8/10/2026, 1:15:54 PM
 1/**
 2 * @param {string} s
 3 * @param {string} t
 4 * @return {boolean}
 5 */
 6var isAnagram = function(s, t) {
-7    if (s.length !== t.length)
-8    {
-9        return false;
-10    }
-11    if(s.split('').sort().join() === t.split('').sort().join())
-12    {
-13        return true;
-14    }
-15    else{
-16        return false;
-17    }
-18};
+7     s = s.split('').sort().join('');
+8     t = t.split('').sort().join('');
+9
+10     console.log(s);
+11     console.log(t);
+12
+13    return (s === t)? true : false;
+14
+15};
