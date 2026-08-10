b1773f36d36fbf4af35324b75553c149b014a3e6
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-10 11:29:35 +0530

    File updated at 8/10/2026, 11:29:34 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.js
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.js
@@ -1,4 +1,4 @@
-// Last updated: 8/10/2026, 11:28:41 AM
+// Last updated: 8/10/2026, 11:29:34 AM
 1/**
 2 * @param {number[]} prices
 3 * @return {number}
@@ -6,28 +6,11 @@
 5var maxProfit = function(prices) {
 6    let profit = 0;
 7    let buy = Infinity;
-8    let sell = prices[prices.length - 1];
-9   for(let i = 0; i < prices.length; i++){
-10
-11    if(prices[i] < buy) {
-12        buy = prices[i];
-13    }
-14
-15    profit = Math.max(profit, (prices[i] - buy));
-16
-17
-18    
-19    // if(prices[i] > buy && (prices.indexOf(buy) < i )){
-20    //     sell = Math.max(sell , prices[i])
-21    // }
-22    // if(prices[i] < buy && (prices.indexOf(sell) > i)){
-23    //     buy = Math.min(buy, prices[i]);
-24    // }
-25
-26    
-27
-28   }
-29   
-30   return (profit > 0 ) ? profit : 0;
-31   
-32};
+8   for(let i = 0; i < prices.length; i++){
+9    if(prices[i] < buy) {
+10        buy = prices[i];
+11    }
+12    profit = Math.max(profit, (prices[i] - buy));
+13 } 
+14   return (profit > 0 ) ? profit : 0; 
+15};
