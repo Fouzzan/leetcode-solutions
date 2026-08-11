c1b085f0a566665239760867b8c9b36f783d8258
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-11 16:14:38 +0530

    File updated at 8/11/2026, 4:14:37 PM

--- 1470-ShuffletheArray/1470-ShuffletheArray.js
+++ 1470-ShuffletheArray/1470-ShuffletheArray.js
@@ -1,4 +1,4 @@
-// Last updated: 8/11/2026, 4:14:22 PM
+// Last updated: 8/11/2026, 4:14:37 PM
 1/**
 2 * @param {number[]} nums
 3 * @param {number} n
@@ -17,12 +17,9 @@
 16            arr2[i - n] = nums[i];
 17        }
 18    }
-19    console.log(arr1);
-20    console.log(arr2);
-21
-22    for(let j = 0; j < n; j++){
-23        result.push(arr1[j]);
-24        result.push(arr2[j]);
-25    }
-26    return result;
-27};
+19    for(let j = 0; j < n; j++){
+20        result.push(arr1[j]);
+21        result.push(arr2[j]);
+22    }
+23    return result;
+24};
