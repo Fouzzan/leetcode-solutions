9eec9ccd41f25aa70f4af920e2638aacfa5c25d0
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-11 07:09:45 +0530

    File updated at 8/11/2026, 7:09:44 AM

--- 242-ValidAnagram/242-ValidAnagram.js
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -1,4 +1,4 @@
-// Last updated: 8/11/2026, 7:08:46 AM
+// Last updated: 8/11/2026, 7:09:44 AM
 1/**
 2 * @param {string} s
 3 * @param {string} t
@@ -7,38 +7,36 @@
 6var isAnagram = function(s, t) {
 7     let map1 = new Map();
 8     let map2 = new Map();
-9     let count = 1;
-10    //  s = s.split('');
+9     
+10    if(s.length !== t.length) return false;
 11
-12    if(s.length !== t.length) return false;
-13
-14    for(let i = 0; i < s.length; i++){
-15        if(map1.has(s[i])){
-16            map1.set(s[i], map1.get(s[i]) + 1);
-17        }
-18        else{
-19             map1.set(s[i], 1);
-20        }
-21       if(map2.has(t[i])){
-22        map2.set(t[i], map2.get(t[i]) + 1);
-23       }
-24       else{
-25        map2.set(t[i], 1)
-26       }
-27    }
-28
-29    console.log(map1);
-30    console.log(map2);
-31    if(map1.size !== map2.size) return false;
-32    for(let [key, value] of map1){
-33        if(!map2.has(key)){
-34            
-35            return false;
-36        }
-37        if(map1.get(key) !== map2.get(key)){
-38            return false;
-39        }
-40    }
-41
-42    return true;
-43};
+12    for(let i = 0; i < s.length; i++){
+13        if(map1.has(s[i])){
+14            map1.set(s[i], map1.get(s[i]) + 1);
+15        }
+16        else{
+17             map1.set(s[i], 1);
+18        }
+19       if(map2.has(t[i])){
+20        map2.set(t[i], map2.get(t[i]) + 1);
+21       }
+22       else{
+23        map2.set(t[i], 1)
+24       }
+25    }
+26
+27    console.log(map1);
+28    console.log(map2);
+29    if(map1.size !== map2.size) return false;
+30    for(let [key, value] of map1){
+31        if(!map2.has(key)){
+32            
+33            return false;
+34        }
+35        if(map1.get(key) !== map2.get(key)){
+36            return false;
+37        }
+38    }
+39
+40    return true;
+41};
