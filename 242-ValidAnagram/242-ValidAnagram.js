b67bbc8c204996f2489d3a6d5942947e6fa4b4f3
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-11 10:38:28 +0530

    File updated at 8/11/2026, 10:38:27 AM

--- 242-ValidAnagram/242-ValidAnagram.js
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -1,40 +1,33 @@
-// Last updated: 8/11/2026, 7:10:01 AM
+// Last updated: 8/11/2026, 10:38:27 AM
 1/**
 2 * @param {string} s
 3 * @param {string} t
 4 * @return {boolean}
 5 */
 6var isAnagram = function(s, t) {
-7     let map1 = new Map();
-8     let map2 = new Map();
-9
-10    if(s.length !== t.length) return false;
-11
-12    for(let i = 0; i < s.length; i++){
-13        if(map1.has(s[i])){
-14            map1.set(s[i], map1.get(s[i]) + 1);
+7     let map = new Map();
+8    if(s.length !== t.length) return false;
+9    for(let i = 0; i < s.length; i++){
+10        if(map.has(s[i])){
+11            map.set(s[i], map.get(s[i]) + 1);
+12        }
+13        else{
+14             map.set(s[i], 1);
 15        }
-16        else{
-17             map1.set(s[i], 1);
-18        }
-19       if(map2.has(t[i])){
-20        map2.set(t[i], map2.get(t[i]) + 1);
-21       }
-22       else{
-23        map2.set(t[i], 1)
-24       }
-25    }
-26
-27    if(map1.size !== map2.size) return false;
-28    for(let [key, value] of map1){
-29        if(!map2.has(key)){
-30            
-31            return false;
-32        }
-33        if(map1.get(key) !== map2.get(key)){
-34            return false;
-35        }
-36    }
-37
-38    return true;
-39};
+16    }
+17    for(let j = 0; j < t.length; j++){
+18            if(map.has(t[j])){
+19            map.set(t[j], map.get(t[j]) - 1);
+20        }
+21        else{
+22            return false;
+23        } 
+24        if(map.get(t[j]) < 0){
+25            return false;
+26        }   
+27    }
+28
+29    return true;
+30
+31      
+32};
