18c25e67360630fc23011fe509aa23f543457d53
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-18 10:11:08 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.js
@@ -0,0 +1,8 @@
+// Last updated: 8/18/2026, 10:11:07 AM
+1/**
+2 * @param {character[]} s
+3 * @return {void} Do not return anything, modify s in-place instead.
+4 */
+5var reverseString = function(s) {
+6    return s.reverse();
+7};
