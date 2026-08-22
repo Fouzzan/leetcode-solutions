1b6255ba16bb124638f1fe75f84f1c31793751f4
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-08-22 11:21:33 +0530

    File updated at 8/22/2026, 11:21:32 AM

--- 125-ValidPalindrome/125-ValidPalindrome.js
+++ 125-ValidPalindrome/125-ValidPalindrome.js
@@ -1,18 +1,18 @@
-// Last updated: 5/27/2026, 7:12:32 AM
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isPalindrome = function(s) {
-    s = s.toLowerCase();
-    const cleanS = s.replace(/[^a-z0-9]/gi, ""); 
-    let reverse = cleanS.split("").reverse().join("");
-    if ( cleanS == reverse)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-};
+// Last updated: 8/22/2026, 11:21:32 AM
+1/**
+2 * @param {string} s
+3 * @return {boolean}
+4 */
+5var isPalindrome = function(s) {
+6    s = s.toLowerCase();
+7    const cleanS = s.replace(/[^a-z0-9]/gi, ""); 
+8    let reverse = cleanS.split("").reverse().join("");
+9    if ( cleanS == reverse)
+10    {
+11        return true;
+12    }
+13    else
+14    {
+15        return false;
+16    }
+17};
