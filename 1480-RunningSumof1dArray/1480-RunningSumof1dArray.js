bc7ace83091d89d599889b2a607ffa608a932300
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-09-04 15:43:25 +0530

    Create 1480-RunningSumof1dArray

---
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.js
@@ -0,0 +1,13 @@
+// Last updated: 9/4/2026, 3:43:24 PM
+1/**
+2 * @param {number[]} nums
+3 * @return {number[]}
+4 */
+5var runningSum = function(nums) {
+6    sum = 0;
+7    for(let i = 0; i < nums.length; i++){
+8        sum += nums[i];
+9        nums[i] = sum;
+10    }
+11    return nums
+12};
