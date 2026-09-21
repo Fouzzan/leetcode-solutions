141a4775d009aea1c767552cd599c35bbcfc9f02
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-09-21 13:33:51 +0530

    Create 118-Pascal'sTriangle

---
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.js
@@ -0,0 +1,26 @@
+// Last updated: 9/21/2026, 1:33:50 PM
+1/**
+2 * @param {number} numRows
+3 * @return {number[][]}
+4 */
+5var generate = function(numRows) {
+6    let result = [];
+7    for (let i = 0;  i < numRows; i++){
+8        result.push([]);
+9        for(let j = 0; j <= i ; j++){
+10            
+11
+12            if(j == 0 || j == i){
+13            result[i][j] = 1;
+14            }
+15            else{
+16                result[i][j] = result[i - 1][j - 1] + result[i-1][j];
+17            }
+18            
+19            
+20        }
+21        console.log(result);
+22    }
+23
+24    return result;
+25};
