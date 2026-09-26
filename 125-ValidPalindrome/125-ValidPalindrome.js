f9bbffab2e6b7096e852cd1c11cb13d03c302a5c
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-09-26 21:53:27 +0530

    File updated at 9/26/2026, 9:53:26 PM

--- 125-ValidPalindrome/125-ValidPalindrome.js
+++ 125-ValidPalindrome/125-ValidPalindrome.js
@@ -1,27 +1,43 @@
-// Last updated: 9/26/2026, 9:24:55 PM
+// Last updated: 9/26/2026, 9:53:26 PM
 1/**
 2 * @param {string} s
 3 * @return {boolean}
 4 */
 5var isPalindrome = function(s) {
 6    s = s.toLowerCase();
-7    let arr = [];
-8    for(let i = 0; i < s.length; i++){
-9        let code = s[i].charCodeAt(0);
+7    let length = s.length;
+8    let left = 0;
+9    let right = length - 1;
 10
-11        if( (code >= 97 && code <= 122) || (code >= 48 && code <= 57)){
-12            arr.push(s[i]);
-13        }
-14    }
-15
-16    if( arr.join('') === arr.reverse().join('')){
-17        // console.log(arr);
-18        // console.log(arr.reverse())
-19        return true;
-20
-21    }
-22    else{
-23        return false;
-24    }
-25
-26};
+11    while (left < right){
+12        let code1 = s[left].charCodeAt(0);
+13        let code2 = s[right].charCodeAt(0);
+14
+15        if( ((code1 >= 97 && code1 <= 122) || (code1 >= 48 && code1 <= 57))){
+16           if( ((code2 >= 97 && code2 <= 122) || (code2 >= 48 && code2 <= 57))){
+17            if(s[left] === s[right] ){
+18            left++;
+19            right--;
+20            }
+21            else{
+22            // console.log(s[left]);
+23            // console.log(s[right])
+24            return false;
+25            }
+26        }
+27        else{
+28            right--;
+29        }
+30           
+31        }
+32        else{
+33            left++;
+34        }
+35        
+36        
+37       
+38        
+39    }
+40    
+41    return true;
+42};
